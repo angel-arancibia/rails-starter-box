c724b5064e255f5ccfae61fac616872e0427055b
Angel <angel.arancibia@gmail.com>
2013-08-06 22:19:53 -0300

    Remove unused config

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -19,9 +19,7 @@ package { [ 'build-essential',
 'git-core', 
 'libxml2', 
 'libxml2-dev', 
-'libxslt1-dev',
-'sqlite3',
-'libsqlite3-dev']:
+'libxslt1-dev']:
 ensure => installed,
 }
 
@@ -43,25 +41,6 @@ class install_mysql {
 }
 class { 'install_mysql': }
 
-class install_postgres {
-  class { 'postgresql': }
-
-  class { 'postgresql::server': }
-
-  pg_user { 'vagrant':
-    ensure    => present,
-    superuser => true,
-    require   => Class['postgresql::server']
-  }
-
-  package { 'libpq-dev':
-    ensure => installed
-  }
-}
-class { 'install_postgres': }
-
-
-
 class install-rvm {
   include rvm
   rvm::system_user { vagrant: ; }
@@ -70,18 +49,12 @@ class install-rvm {
     'ruby-1.9.3-p194':
       ensure => 'present',
       default_use => false;
-    'ruby-1.8.7-p370':
-      ensure => 'present',
-      default_use => false;
   }
 
   rvm_gem {
     'ruby-1.9.3-p194/bundler': ensure => latest;
     'ruby-1.9.3-p194/rails': ensure => latest;
     'ruby-1.9.3-p194/rake': ensure => latest;
-    'ruby-1.8.7-p370/bundler': ensure => latest;
-    'ruby-1.8.7-p370/rails': ensure => latest;
-    'ruby-1.8.7-p370/rake': ensure => latest;
   }
 
 }
